41d1f702d7f0e892f0ebe6d7d183792da04c7f88
carlosrene.flores.mendive@bbva.com <mi36597@MI36597MAC.local>
2026-07-14 10:43:13 -0600

    add prints

--- Asignacion/update_historico.py
+++ Asignacion/update_historico.py
@@ -134,12 +134,7 @@ def _update_from_torre_v2(
     update_df_torre_v2_cerr["asesor credito"] = update_df_torre_v2_cerr["asesor credito tcv2"]
     update_df_torre_v2_cerr["estatus de lead"] = update_df_torre_v2_cerr["estatus de lead tcv2"]
 
-    print(f"update_df_torre_v2_cerr shape: {update_df_torre_v2_cerr.shape[0]}")
-    print(update_df_torre_v2_cerr[["id lead", "estatus de lead", "estatus de lead tcv2", 
-                                   "espacio automarket","espacio automarket tcv2", 
-                                   "asesor espacio", "asesor espacio tcv2",
-                                   "asesor credito", "asesor credito tcv2",
-                                   "flag salio de cerrado"]])
+    
     update_df_torre_v2_cerr = update_df_torre_v2_cerr.drop(
         columns=["estatus de lead tcv2", "asesor espacio tcv2", "espacio automarket tcv2", "asesor credito tcv2"]
     )
@@ -268,8 +263,10 @@ def actualizar_historico_leads(
 
     if create_timestamped_copy:
         create_csv_file_in_drive_folder(drive, historico_folder_id, resultado_final, timestamped_filename)
+        print(f'Copia del historico timestamp... OK')
     if write_outputs:
         write_csv_to_drive(drive, output_file_id, resultado_final)
+        print(f'Escritura del historico... OK')
 
     result: Dict[str, Any] = {
         "resultado_final": resultado_final,
